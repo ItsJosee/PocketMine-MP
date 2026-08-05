426a7d1b68fff438b674a1ccc35c816c34742f8b
Bareww <joseemilio.2720@gmail.com>
2026-08-05 10:40:12 +0200

    Revert Cereal migration - only update protocol version to 2168

--- src/network/mcpe/cache/CraftingDataCache.php
+++ src/network/mcpe/cache/CraftingDataCache.php
@@ -192,6 +192,6 @@ final class CraftingDataCache{
 		}
 
 		Timings::$craftingDataCacheRebuild->stopTiming();
-		return CraftingDataPacket::createFromMixed($recipesWithTypeIds, $potionTypeRecipes, $potionContainerChangeRecipes, [], true);
+		return CraftingDataPacket::create($recipesWithTypeIds, $potionTypeRecipes, $potionContainerChangeRecipes, [], true);
 	}
 }
